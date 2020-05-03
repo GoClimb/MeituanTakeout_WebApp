769b4edfe9f2a30f628a0bfb2cb38920e4b98d8a
Jy <jygoclimb@gmail.com>
2020-05-04 00:25:34 +0800

    详情页基本完成

--- src/views/Details/index.vue
+++ src/views/Details/index.vue
@@ -19,7 +19,13 @@
           <div class="header-content-mid">公告：Jy真帅～～</div>
 
           <div class="header-content-bottom">
-            <Carousel height="17px" direction="vertical" :autoplay="true" :loop='true' indicator-position='none'>
+            <Carousel
+              height="17px"
+              direction="vertical"
+              :autoplay="true"
+              :loop="true"
+              indicator-position="none"
+            >
               <CarouselItem v-for="item in 2" :key="item">
                 <i
                   style="background-image: url('https://p0.meituan.net/activityconfig/6087b33fd42d14fd94e899084aaef56d1720.png')"
@@ -29,7 +35,6 @@
               </CarouselItem>
             </Carousel>
           </div>
-
         </div>
       </div>
     </div>
@@ -37,36 +42,65 @@
 
     <!--详情页主体内容 start-->
     <div class="details-main">
-      <Tabs v-model="active" animated :duration='0.3' line-width='20'>
-
+      <Tabs v-model="active" animated :duration="0.3" line-width="20">
         <Tab title="点菜">
           <div class="order">
+            <!--左侧导航栏start-->
             <nav>
-              <div v-for='item in 20' :key='item' class="nav-list">
+              <div v-for="item in 20" :key="item" class="nav-list">
                 <span>
-                  <img src="" alt="">
+                  <img src alt />
                   热销
                 </span>
               </div>
             </nav>
+            <!--左侧导航栏end-->
+
+            <!--右侧内容区start-->
             <div class="order-right">
-              <div>热销</div>
-              <div>
-                <div>
+              <div class="order-right-fixed">热销</div>
+              <div class="order-right-content">
+                <div class="order-right-content-list" v-for="item in 5" :key="item">
                   <dl>
                     <dt>热销</dt>
-                    <dd>
-                      <div><img src="https://p1.meituan.net/xianfu/11daa06ba3895630b988fb3f02d9042a200485.jpg@225w_225h_80Q_0e_1l.webp" alt=""></div>
-                      <div>sss</div>
+                    <dd v-for="item in 5" :key="item">
+                      <div class="logo">
+                        <img
+                          src="https://p1.meituan.net/xianfu/11daa06ba3895630b988fb3f02d9042a200485.jpg@225w_225h_80Q_0e_1l.webp"
+                          alt
+                        />
+                      </div>
+                      <div class="content">
+                        <div class="content-header">麦辣鸡腿</div>
+                        <div class="content-text">麦辣椒1麦辣椒1麦辣椒1麦辣椒1麦辣椒1麦辣椒1麦辣椒1</div>
+                        <div class="content-sales">
+                          <span>月售300</span>
+                          <span>赞20</span>
+                        </div>
+                        <div class="content-piece">
+                          <div>¥ 5</div>
+                          <div>22.5</div>
+                          <button></button>
+                        </div>
+                        <div class="discount">
+                          <div>
+                          <span>2.22折 限1份</span>
+                          </div>
+                        </div>
+                      </div>
                     </dd>
                   </dl>
                 </div>
               </div>
             </div>
+            <!--右侧内容区end-->
           </div>
         </Tab>
 
         <Tab title="评价">
+          <div>评价</div>
+          <div>评价</div>
+          <div>评价</div>
           <div>评价</div>
         </Tab>
         <Tab title="商家">
@@ -75,6 +109,28 @@
       </Tabs>
     </div>
     <!--详情页主体内容 end-->
+    <div class="details-bottom">
+
+      <div class="details-bottom-logo">
+        <div v-if='false'>1</div>
+      </div>
+
+      <div class="details-bottom-content">
+
+        <div class="details-bottom-content-confirm" v-if="false">
+          <div>
+            <span>¥ 5</span>
+            <span>¥22.5</span>
+          </div>
+          <span>另需配送费¥9</span>
+        </div>
+
+        <span v-else>另需配送费¥9</span>
+
+      </div>
+
+      <div class="details-bottom-right">¥0起送</div>
+    </div>
   </div>
 </template>
 
@@ -101,8 +157,7 @@ export default {
     }
   },
 
-  methods: {
-  }
+  methods: {}
 }
 </script>
 
@@ -178,12 +233,12 @@ export default {
         position: relative;
         &::after {
           position: absolute;
-          content: ' ';
+          content: " ";
           width: 5px;
           height: 9px;
           right: 10px;
           top: 3px;
-          background-image: url('~@/assets/icon/youjiantou.png');
+          background-image: url("~@/assets/icon/youjiantou.png");
           background-size: 5px 9px;
           background-repeat: no-repeat;
         }
@@ -194,14 +249,14 @@ export default {
           height: 15px;
           display: flex;
           align-items: center;
-        i {
-          display: inline-block;
-          width: 15px;
-          height: 15px;
-          background-size: 100%;
-          margin-right: 5px;
+          i {
+            display: inline-block;
+            width: 15px;
+            height: 15px;
+            background-size: 100%;
+            margin-right: 5px;
+          }
         }
-      }
       }
     }
   }
@@ -213,16 +268,188 @@ export default {
       width: 80px;
       height: 600px;
       overflow-y: auto;
+      background: #f5f5f5;
+      font-size: 13.5px;
+      font-weight: normal;
       div {
         min-width: 80px;
-        padding: 10px;
+        min-height: 49px;
+        padding: 18px;
         box-sizing: border-box;
       }
+      .active {
+        background: white;
+      }
     }
-    div {
+    .order-right {
       flex: 1;
+      height: 100%;
+      overflow: hidden;
+      position: relative;
+      .order-right-fixed {
+        width: 100%;
+        height: 36px;
+        line-height: 36px;
+        padding-left: 10px;
+        box-sizing: border-box;
+        font-size: 13px;
+        position: absolute;
+        top: 0;
+        color: #333333;
+        background: white;
+        z-index: 1;
+      }
+      .order-right-content {
+        height: 688px;
+        overflow: scroll;
+        .order-right-content-list {
+          dl {
+            dt {
+              width: 100%;
+              height: 36px;
+              line-height: 36px;
+              padding-left: 10px;
+              box-sizing: border-box;
+              font-size: 13px;
+            }
+            dd {
+              display: flex;
+              margin-top: 10px;
+              .logo {
+                width: 75px;
+                img {
+                  width: 100%;
+                }
+              }
+              .content {
+                width: 220px;
+                .content-header {
+                  color: #333333;
+                  font-weight: bold;
+                  line-height: 22.5px;
+                }
+                .content-text {
+                  color: #666666;
+                  font-size: 11px;
+                  line-height: 15px;
+                  white-space: nowrap;
+                  text-overflow: ellipsis;
+                  overflow: hidden;
+                  word-break: break-all;
+                }
+                .content-sales {
+                  span {
+                    color: #666;
+                    font-size: 11px;
+                    line-height: 15px;
+                    margin-right: 8px;
+                  }
+                }
+                .content-piece {
+                  width: 100%;
+                  // line-height: 26px;
+                  // display: flex;
+                  position: relative;
+                  > div:nth-child(1) {
+                    display: inline-block;
+                    color: #fb4e44;
+                    font-size: 18px;
+                    height: 21.5px;
+                    line-height: 21.5px;
+                    vertical-align: middle;
+                  }
+                  > div:nth-child(2) {
+                    display: inline-block;
+                    color: #a9a9a9;
+                    font-size: 11px;
+                    text-decoration: line-through;
+                    margin-left: 4px;
+                  }
+                  button {
+                    display: inline-block;
+                    width: 25px;
+                    height: 25px;
+                    background-image: url("~@/assets/icon/add.png");
+                    background-size: 100%;
+                    background-repeat: no-repeat;
+                    border: none;
+                    border-radius: 50%;
+                    position: absolute;
+                    right: 0;
+                  }
+                }
+
+                .discount {
+                  div {
+                    width: 80px; height: 15px;
+                    position: relative;
+                    font-size: 11px;
+                    text-align: center;
+                    line-height: 15px;
+                    color: #fb4e44;
+                     &::before {
+                      content: '';
+                      position: absolute;
+                      top: -50%;
+                      bottom: -50%;
+                      left: -50%;
+                      right: -50%;
+                      -webkit-transform: scale(0.5);
+                      transform: scale(0.5);
+                      border-style: solid;
+                      border-width: 1px;
+                      border-color: #fb4e44;
+                      border-radius: 0;
+                  }
+                 }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+  .details-bottom {
+    width: 100%; height: 50px;
+    position: fixed;
+    bottom: 0; left: 0;
+    background: #3b3b3c;
+    z-index: 2;
+    display: flex;
+    align-items: center;
+    .details-bottom-logo {
+      width: 50px; height: 50px;
+      position: relative;
+      top: -10px;
+      border-radius: 50%;
+      background: url('~@/assets/icon/details-bottom.png') no-repeat;
+      background-size: contain;
+      margin: 0 10px;
+    }
+    .details-bottom-logo-active {
+      width: 50px; height: 50px;
+      position: relative;
+      top: -10px;
+      border-radius: 50%;
+      background: url('~@/assets/icon/details-bottom-active.png') no-repeat;
+      background-size: contain;
+      margin: 0 10px;
+    }
+    .details-bottom-content {
+      flex: 1 1 auto;
+      span {
+        font-size: 14px;
+        color: #999;
+      }
+    }
+    .details-bottom-right {
+      padding: 0 18px;
+      line-height: 50px;
+      font-size: 16px;
+      font-weight: bold;
+      color: #999;
     }
   }
 }
-
 </style>
